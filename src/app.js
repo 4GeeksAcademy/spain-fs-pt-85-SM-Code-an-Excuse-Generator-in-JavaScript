44d680c5fbc9f51188c1f13e7b9d2cbb9b99af39
unduashe <unduashe@outlook.es>
2024-10-28 19:05:12 +0000

    modificados app.js e index.html para la generación de excusa y su visibilidad en el navegador

--- src/app.js
+++ src/app.js
@@ -1,11 +1,34 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  excuseGenerator();
 };
+
+let who = ["El autobusista", "Ella", "Mi abuela", "Mi lapiz", "Mi perro"];
+let action = [
+  " se ha comido",
+  " ha destruido",
+  " ha teletransportado",
+  " ha sacrificado a las fuerzas del caos"
+];
+let what = [
+  " mis deberes",
+  " el autobús en el que iba",
+  " mi teléfono",
+  " a Paco, mi vecino del cuarto"
+];
+let when = [
+  " cuando estaba en el baño.",
+  " mientras comía.",
+  " mientras lloraba desconsoladamente sin poder hacer nada para impedirlo.",
+  " cuando estaba durmiendo."
+];
+
+function excuseGenerator() {
+  let excuse = `${who[Math.floor(Math.random() * who.length)]}${
+    action[Math.floor(Math.random() * action.length)]
+  }${what[Math.floor(Math.random() * what.length)]}${
+    when[Math.floor(Math.random() * when.length)]
+  }`;
+
+  document.getElementById("generatedExcuse").innerText = excuse;
+}
