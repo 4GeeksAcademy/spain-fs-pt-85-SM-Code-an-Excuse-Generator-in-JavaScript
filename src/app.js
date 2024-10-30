0d5d313b521d8ef9a2d1e75adff1cb52bd7e3ab6
unduashe <unduashe@outlook.es>
2024-10-30 19:22:59 +0000

    modified the excuseGenerator function to make it easyer to read, added a new function to get a random item of an array

--- src/app.js
+++ src/app.js
@@ -14,7 +14,7 @@ let what = [
   " mis deberes",
   " el autobús en el que iba",
   " mi teléfono",
-  " a Paco, mi vecino del cuarto"
+  " a Paco, mi vecino del cuarto,"
 ];
 let when = [
   " cuando estaba en el baño.",
@@ -23,12 +23,24 @@ let when = [
   " cuando estaba durmiendo."
 ];
 
+function randomItemOfArray(arr) {
+  return arr[Math.floor(Math.random() * arr.length)];
+}
+
 function excuseGenerator() {
-  let excuse = `${who[Math.floor(Math.random() * who.length)]}${
-    action[Math.floor(Math.random() * action.length)]
-  }${what[Math.floor(Math.random() * what.length)]}${
-    when[Math.floor(Math.random() * when.length)]
-  }`;
+  let excuse = `${randomItemOfArray(who)}${randomItemOfArray(action)
+  }${randomItemOfArray(what)}${randomItemOfArray(when)}`;
 
   document.getElementById("generatedExcuse").innerText = excuse;
 }
+
+// function excuseGenerator() {
+
+//   let excuse = `${who[Math.floor(Math.random() * who.length)]}${
+//     action[Math.floor(Math.random() * action.length)]
+//   }${what[Math.floor(Math.random() * what.length)]}${
+//     when[Math.floor(Math.random() * when.length)]
+//   }`;
+
+//   document.getElementById("generatedExcuse").innerText = excuse;
+// }
